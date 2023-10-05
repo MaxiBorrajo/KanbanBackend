ef61c0c48eed93e7443921870cccf402431179fa
MaxiBorrajo <maximilianoborrajo0@gmail.com>
2023-10-05 16:17:50 -0300

    primera version terminada

--- src/middlewares/authMiddleware.js
+++ src/middlewares/authMiddleware.js
@@ -2,8 +2,7 @@ import CustomError from "../utils/customError.js";
 
 function authMiddleware(req, res, next) {
   try {
-    console.log(req.session.cookie)
-    if (req.isAuthenticated()) {
+    if (req.session.cookie) {
       next();
     } else {
       throw new CustomError("Invalid authorization", 401);
